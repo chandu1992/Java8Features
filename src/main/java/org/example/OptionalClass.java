9adaac8346086be85a21b1b04be728e5f5a8b270
chandrashekharbhaupande <chpande55@gmail.com>
2025-02-27 22:23:24 +0530

    optionl class of method

--- src/main/java/org/example/OptionalClass.java
+++ src/main/java/org/example/OptionalClass.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class OptionalClass {
     public static void main(String[] args) {
         Optional<String> emptyOptional = Optional.empty(); //create empty Optional class.
         System.out.println(emptyOptional);
+
+        List<Integer> arrayData = new ArrayList<>(Arrays.asList(10,20,30,40));
+        // Correct way to create an Optional of a List
+        Optional<List<Integer>> optional = Optional.of(arrayData);
+
+        System.out.println(optional);
     }
 }
