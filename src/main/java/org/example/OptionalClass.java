e4c04a666561751818d4b04287e1f730c187c9c2
chandrashekharbhaupande <chpande55@gmail.com>
2025-02-27 22:10:20 +0530

    optional class

---
+++ src/main/java/org/example/OptionalClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Optional;
+
+public class OptionalClass {
+    public static void main(String[] args) {
+        Optional<String> emptyOptional = Optional.empty(); //create empty Optional class.
+        System.out.println(emptyOptional);
+    }
+}
