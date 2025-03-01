d5a801de0dfe903db5d4230482533d53c9b7a1f4
chandrashekharbhaupande <chpande55@gmail.com>
2025-03-01 11:04:13 +0530

    optional methods

--- src/main/java/org/example/OptionalClassMethods.java
+++ src/main/java/org/example/OptionalClassMethods.java
@@ -10,7 +10,13 @@ public class OptionalClassMethods {
 
         List<Integer> arrayData = new ArrayList<>(Arrays.asList(10,20,30,40));
         // Correct way to create an Optional of a List
-        Optional<List<Integer>> optional = Optional.of(arrayData);
+        Optional<List<Integer>> optional = Optional.ofNullable(arrayData);
+        System.out.println( optional.isPresent()); // if data present it return true
+
+        Optional<List<Integer>> optional1 = Optional.ofNullable(null);
+        System.out.println( optional1.isPresent());
+
+        optional.ifPresent(data -> System.out.println(data));
 
     }
 }
